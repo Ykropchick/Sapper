866f476a56f0841f0446630bc610eb4a25ea8166
Kirill <shkolenko02-03-2004@mail.ru>
2022-05-16 08:45:59 +0300

    Draw the borad

--- main.py
+++ main.py
@@ -10,13 +10,15 @@ class Main:
         pg.init()
         self.Width = 600
         self.Height = 600
-        self.Rows = 30
-        self.Cols = 30
+        self.Rows = 15
+        self.Cols = 15
         self.Mines = 15
+        self.size = self.Width // self.Rows
         self.screen = pg.display.set_mode((self.Width, self.Height))
-        self.clock = pg.time.Clock
+        self.clock = pg.time.Clock()
         self.field = []
         self.running = True
+        self.FPS = 30
 
     def matrix_field(self):
         self.field = [[0 for _ in range(self.Cols)] for _ in range(self.Rows)]
@@ -28,15 +30,24 @@ class Main:
             mine_position.add((row, col))
             self.field[row][col] = -1
 
-    def do_board(self):
-        pass
+    def draw_board(self):
+        for i in range(self.Cols):
+            y = i * self.size
+            for j in range(self.Rows):
+                x = j * self.size
+                pg.draw.rect(self.screen, "gray", (x, y, self.size, self.size))
+                pg.draw.rect(self.screen, "black", (x, y, self.size, self.size), 2)
 
     def mainloop(self):
         self.matrix_field()
         while self.running:
+            # self.screen.fill("white")
+            self.draw_board()
             for event in pg.event.get():
                 if event.type == pg.QUIT:
                     self.running = False
+            self.clock.tick(self.FPS)
+            pg.display.update()
 
 
 main = Main()
